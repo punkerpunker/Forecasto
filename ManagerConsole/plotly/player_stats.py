e9b896db72ead375bb20cd0482a46723b3470659
punkerpunker <vazheningleb@yandex.ru>
2020-12-24 02:18:25 +0300

    commit

--- ManagerConsole/plotly/player_stats.py
+++ ManagerConsole/plotly/player_stats.py
@@ -2,8 +2,8 @@ import plotly.graph_objs as go
 
 
 def create_stats_graph(player_stats_df):
-    stats = player_stats_df.sort_values('season')
-    year = stats.season.tolist()
+    stats = player_stats_df.groupby(['season'])['goals', 'assists', 'games'].sum().reset_index().sort_values('season')
+    year = [str(x)[2:] for x in stats.season.tolist()]
 
     # Внешний вид
     fig = go.Figure(
@@ -14,7 +14,7 @@ def create_stats_graph(player_stats_df):
                     size=22,
                     color="white"
                 )),
-            xaxis=dict(tickformat='d'),
+            xaxis=dict(type='category'),
             paper_bgcolor='rgba(0,0,0,0.0)',
             plot_bgcolor='rgba(0,0,0,0.0)',
             modebar=dict(bgcolor='rgba(0,0,0,0.0)', color='white', activecolor='rgba(243, 38, 130, 0.3)'),
@@ -28,14 +28,11 @@ def create_stats_graph(player_stats_df):
     fig.update_yaxes(tickfont=dict(family='Verdana', size=18, color='white'))
 
     # Данные
-    fig.add_trace(go.Bar(x=year, y=stats.goals.tolist(), name='Goals', marker_color='rgba(243, 38, 130, 0.5)'
-                         ))
-    fig.add_trace(go.Bar(x=year, y=stats.assists.tolist(), name='Assists',  marker_color='rgba(173, 216, 230, 0.8)'
-                         ))
-    fig.add_trace(go.Scatter(x=year, y=stats.games.tolist(), name='Games',
-                             line=dict(color='royalblue', width=3)))
+    fig.add_trace(go.Bar(x=year, y=stats.goals.tolist(), name='Goals', marker_color='rgba(243, 38, 130, 0.5)'))
+    fig.add_trace(go.Bar(x=year, y=stats.assists.tolist(), name='Assists',  marker_color='rgba(173, 216, 230, 0.8)'))
+    fig.add_trace(go.Scatter(x=year, y=stats.games.tolist(), name='Games', line=dict(color='royalblue', width=3)))
     # Стакаем голы и ассисты
     fig.update_layout(barmode='stack')
 
-    graph = fig.to_html(full_html=False)
+    graph = fig.to_html(full_html=True)
     return graph
