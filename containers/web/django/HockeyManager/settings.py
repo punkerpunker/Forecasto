29023abd97ca7b8ce76444e1aebf87d2e19764af
punkerpunker <vazheningleb@yandex.ru>
2020-12-30 23:16:39 +0300

    env files

--- containers/web/django/HockeyManager/settings.py
+++ containers/web/django/HockeyManager/settings.py
@@ -126,5 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/app/HockeyManager/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+print(STATIC_ROOT, flush=True)
+
 LOGIN_REDIRECT_URL = '/'
